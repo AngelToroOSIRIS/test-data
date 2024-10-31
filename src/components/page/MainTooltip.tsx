998a16f4e9ae845819238b6d23ba1e372929f250
AngelToroOsiris <angel.toro@escuelaing.edu.co>
2024-10-31 12:39:12 -0500

    tooltip component finish

--- src/components/page/MainTooltip.tsx
+++ src/components/page/MainTooltip.tsx
@@ -1,8 +1,20 @@
 import Title from "@/components/ui/Title";
-import { Divider } from "@nextui-org/react";
+import { Button, Divider } from "@nextui-org/react";
 import { Code } from "@nextui-org/code";
+import Tooltip from "@/components/ui/Tooltip";
+import { useState } from "react";
+import InputForm from "@/components/forms/InputForm";
+import Icon from "@/components/ui/Icon";
+import { Radio, RadioGroup } from "@nextui-org/radio";
 
 const MainTooltip = () => {
+  const [content, setContent] = useState<string>("Tooltip content");
+  const [text, setText] = useState<String>("Tooltip text");
+  const [icon, setIcon] = useState<string>("info-circle");
+  const [showContent, setShowContent] = useState<"icon" | "text" | "node">(
+    "node",
+  );
+
   return (
     <div className="flex flex-col md:flex-row md:justify-between w-full max-w-[1000px] mx-auto gap-2">
       <div className="flex flex-col gap-3 bg-background rounded-large p-4 w-full max-w-[900px] ">
@@ -13,16 +25,78 @@ const MainTooltip = () => {
           center
         />
         <Divider className="w-[95%] bg-divider mx-auto" />
+        <div className="flex mx-auto my-8">
+          <Tooltip content={content}>
+            {showContent == "icon" && (
+              <Icon
+                icon={icon}
+                className="text-default-400 text-center transition-all hover:text-default-foreground text-4xl"
+              />
+            )}
+            {showContent == "text" && (
+              <p className="text-lg text-center">{text}</p>
+            )}
+            {showContent == "node" && <Button>Hover me</Button>}
+          </Tooltip>
+        </div>
       </div>
       <div className="flex flex-col gap-4 bg-background w-full md:w-[85%] max-w-[800px] rounded-large p-4">
         <Title text="Propiedades" size="medium" />
-        <Code className="flex flex-col w-full mx-auto">
-          <p>{"icon: string;"}</p>
-          <p>{"title?: string;"}</p>
-          <p>{"onClick?: () => void;"}</p>
-          <p>{"className?: string;"}</p>
+        <Code className="flex flex-col w-full mx-auto overflow-x-auto">
+          <p>{"content: string | React.ReactNode;"}</p>
+          <p>{"children: React.ReactNode;"}</p>
+          <p>
+            classNames?: {"{"} <br />
+            mobile?: string, <br />
+            desktop?: string, trigger?: string; <br />
+            {"}"}
+          </p>
         </Code>
         <Divider className="w-[95%] bg-divider mx-auto" />
+        <Title text="Test" size="medium" primary />
+        <InputForm
+          name="icon"
+          type="text"
+          onChange={({ value }) =>
+            value ? setContent(String(value)) : setContent("Tooltip text")
+          }
+          placeholder="Ingrese content"
+          label={{ value: "Content", required: false }}
+        />
+        <RadioGroup
+          label="Item"
+          className="text-center mx-auto"
+          orientation="horizontal"
+          value={showContent}
+          // @ts-ignore
+          onValueChange={setShowContent}
+        >
+          <Radio value="node">Node</Radio>
+          <Radio value="icon">Icon</Radio>
+          <Radio value="text">Text</Radio>
+        </RadioGroup>
+        {showContent == "icon" && (
+          <InputForm
+            name="icon"
+            type="text"
+            onChange={({ value }) =>
+              value ? setIcon(String(value)) : setIcon("info-circle")
+            }
+            placeholder="Ingrese icon"
+            label={{ value: "Icon", required: false }}
+          />
+        )}
+        {showContent == "text" && (
+          <InputForm
+            name="text"
+            type="text"
+            onChange={({ value }) =>
+              value ? setText(String(value)) : setText("Title component")
+            }
+            placeholder="Ingrese texto"
+            label={{ value: "Text", required: false }}
+          />
+        )}
       </div>
     </div>
   );

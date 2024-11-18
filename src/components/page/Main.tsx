727331f5319985ca6ffc5179a486449bb8ef233b
AngelToroOsiris <angel.toro@escuelaing.edu.co>
2024-11-18 15:07:10 -0500

    update text empty cardComponents

--- src/components/page/Main.tsx
+++ src/components/page/Main.tsx
@@ -26,32 +26,44 @@ import MainTextareaForm from "@/components/page/MainTextareaForm";
 import MainAutoCompleteForm from "@/components/page/MainAutoCompleteForm";
 import MainSuggestInput from "@/components/page/MainSuggestInput";
 
+const components: { name: string; state: StatesComponents }[] = [
+  { name: "AnimateText", state: "Terminado" },
+  { name: "SuggestInputForm", state: "Terminado" },
+  { name: "AutoCompleteForm", state: "Terminado" },
+  { name: "InputForm", state: "Terminado" },
+  { name: "SelectForm", state: "Terminado" },
+  { name: "TextareaForm", state: "Terminado" },
+  { name: "Title", state: "Terminado" },
+  { name: "Icon", state: "Terminado" },
+  { name: "Password", state: "Terminado" },
+  { name: "Tooltip", state: "Terminado" },
+  { name: "SideMenu", state: "Terminado" },
+  { name: "Modal", state: "Terminado" },
+  { name: "Button", state: "Terminado" },
+  { name: "Steps", state: "Terminado" },
+  { name: "Carrousel", state: "Terminado" },
+  { name: "DragContainerModal", state: "Terminado" },
+];
+
 const Main = () => {
   const [select, setSelect] = useState<string>("all");
   const [isOpen, setIsOpen] = useState(false);
   const [showInput, setShowInput] = useState(false);
   const [searchComponent, setSearchComponent] = useState<string | null>(null);
-
-  const components: { name: string; state: StatesComponents }[] = [
-    { name: "AnimateText", state: "Terminado" },
-    { name: "SuggestInputForm", state: "Terminado" },
-    { name: "AutoCompleteForm", state: "Terminado" },
-    { name: "InputForm", state: "Terminado" },
-    { name: "SelectForm", state: "Terminado" },
-    { name: "TextareaForm", state: "Terminado" },
-    { name: "Title", state: "Terminado" },
-    { name: "Icon", state: "Terminado" },
-    { name: "Password", state: "Terminado" },
-    { name: "Tooltip", state: "Terminado" },
-    { name: "SideMenu", state: "Terminado" },
-    { name: "Modal", state: "Terminado" },
-    { name: "Button", state: "Terminado" },
-    { name: "Steps", state: "Terminado" },
-    { name: "Carrousel", state: "Terminado" },
-    { name: "DragContainerModal", state: "Terminado" },
-  ];
+  const [filterComponents, setFilterComponents] =
+    useState<{ name: string; state: StatesComponents }[]>(components);
 
   components.sort((a, b) => a.name.localeCompare(b.name));
+
+  const filterComponentsFn = () => {
+    const newArray = components.filter((i) =>
+      searchComponent
+        ? i.name.toLowerCase().includes(searchComponent.toLowerCase())
+        : i,
+    );
+
+    setFilterComponents(newArray);
+  };
 
   useEffect(() => {
     const handleKeyDown = (event: KeyboardEvent) => {
@@ -80,6 +92,12 @@ const Main = () => {
       window.removeEventListener("keydown", handleKeyDown);
     };
   }, [showInput, isOpen]);
+
+  useEffect(() => {
+    filterComponentsFn();
+  }, [searchComponent]);
+
+  console.log(searchComponent);
 
   return (
     <>
@@ -118,11 +136,11 @@ const Main = () => {
                     type="search"
                     className="outline-none"
                     placeholder="Buscar componente"
-                    onChange={({ value }) =>
+                    onChange={({ value }) => {
                       value
                         ? setSearchComponent(String(value))
-                        : setSearchComponent(null)
-                    }
+                        : setSearchComponent(null);
+                    }}
                     label={{ value: "Buscar componente", required: false }}
                   />
                 </motion.div>
@@ -130,25 +148,23 @@ const Main = () => {
             </AnimatePresence>
             <div className="flex flex-wrap mx-auto justify-center items-center gap-4">
               <AnimatePresence>
-                {components
-                  .filter((i) =>
-                    searchComponent
-                      ? i.name
-                          .toLowerCase()
-                          .includes(searchComponent.toLowerCase())
-                      : i,
-                  )
-                  .map((component, i) => (
-                    <CardComponent
-                      key={i}
-                      name={component.name}
-                      state={component.state}
-                      setSelected={setSelect}
-                    />
-                  ))}
+                {filterComponents.map((component, i) => (
+                  <CardComponent
+                    key={i}
+                    name={component.name}
+                    state={component.state}
+                    setSelected={setSelect}
+                  />
+                ))}
               </AnimatePresence>
             </div>
           </div>
+        )}
+
+        {filterComponents.length < 1 && (
+          <p className="text-default-foreground text-center select-none text-xl md:text-2xl font-semibold">
+            Aquí no hay nada.
+          </p>
         )}
 
         {select === "title" && <MainTitle />}
@@ -178,7 +194,7 @@ const Main = () => {
 
         {select == "all" && (
           <motion.div
-            initial={{ opacity: 0.5, width: 50 }}
+            initial={{ opacity: 0.6, width: 50 }}
             whileHover={{ opacity: 1, width: 150 }}
             className="hidden fixed right-3 bottom-14 select-none overflow-x-hidden text-sm md:flex flex-col gap-2 bg-default rounded-lg p-2"
           >
